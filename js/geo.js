66e28647f3118fc44c19d6721fb2904bf4e1c8e4
Jesse Chen <github@jessechen.net>
2012-02-04 04:05:33 -0800

    geo geo geo

--- js/geo.js
+++ js/geo.js
@@ -12,13 +12,10 @@ function initiate_geolocation() {
 			
 function handle_geolocation_query(position) {
 	var image_url = "http://maps.google.com/maps/api/staticmap?sensor=false&center=" + position.coords.latitude + "," +  
-                    position.coords.longitude + "&zoom=14&size=300x400&markers=color:blue|label:S|" +  
+                    position.coords.longitude + "&zoom=16&size=300x400&markers=color:blue|label:S|" +  
                     position.coords.latitude + ',' + position.coords.longitude;  
   
-    jQuery("#map").remove();  
-    jQuery(document.body).append(  
-        jQuery(document.createElement("img")).attr("src", image_url).attr('id','map')  
-    );  
+	jQuery(document.getElementById("map")).attr("src", image_url);
 }
 			
 function handle_errors(error) {
