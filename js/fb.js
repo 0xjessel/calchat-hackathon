47a9b6f328b5075983fb789d9f62d47e4c81678b
Jesse Chen <github@jessechen.net>
2012-02-03 20:19:16 -0800

    trying to add fb script to 'fb-root'

--- js/fb.js
+++ js/fb.js
@@ -1,9 +1,3 @@
-(function() {
-	var e = document.createElement('script'); e.async = true;
-	e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-	document.getElementById('fb-root').appendChild(e);
-}());
-
 window.fbAsyncInit = function() {
 	FB.init({ appId: '297402036983700', 
 	status: true, 
